d81a857b909a2bde20767dfe0f6f3d4a6e630747
WWEaboo <ethan.welsh7149@gmail.com>
2022-05-16 15:51:24 +0100

    Added Create Auction form

--- client/src/App.js
+++ client/src/App.js
@@ -91,16 +91,17 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
+        
+        <div className="form-create-auction">
+          <h2>Create Auction</h2>
+          <div>
+            Bidding Time <input type="text" ref={x => this.biddingTime = x} defaultValue={60} />
+          </div>
+          <div>
+            TTP Address <input type="text" ref={x => this.ttp = x} defaultValue={'0x0000000000000000000000000000000000000000'} />
+          </div>
+          <button onClick={this.newAuction}>Create Auction</button>
+        </div>
 
         <div>
           <table>
